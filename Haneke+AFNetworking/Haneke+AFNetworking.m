b16e84b1f46d36d2385147e31a5821c77539459d
Hermes Pique <hermespique@gmail.com>
2014-08-30 20:56:52 +0200

    Clarify license

--- Haneke+AFNetworking/Haneke+AFNetworking.m
+++ Haneke+AFNetworking/Haneke+AFNetworking.m
@@ -5,6 +5,18 @@
 //  Created by Hermes Pique on 8/30/14.
 //  Copyright (c) 2014 Hermes Pique. All rights reserved.
 //
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
 
 #import "Haneke+AFNetworking.h"
 #import <AFNetworking/AFNetworking.h>
@@ -57,12 +69,12 @@
 + (void)load {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        [self swizzleSelector:@selector(fetchImageWithSuccess:failure:) withSelector:@selector(af_fetchImageWithSuccess:failure:)];
-        [self swizzleSelector:@selector(cancelFetch) withSelector:@selector(af_cancelFetch)];
+        [self af_swizzleSelector:@selector(fetchImageWithSuccess:failure:) withSelector:@selector(af_fetchImageWithSuccess:failure:)];
+        [self af_swizzleSelector:@selector(cancelFetch) withSelector:@selector(af_cancelFetch)];
     });
 }
 
-+ (void)swizzleSelector:(SEL)originalSelector withSelector:(SEL)swizzledSelector
++ (void)af_swizzleSelector:(SEL)originalSelector withSelector:(SEL)swizzledSelector
 {
     Class class = self.class;
     Method originalMethod = class_getInstanceMethod(class, originalSelector);
